59567f3b4244dba781de0e3fb0b32a98807a6c9b
Maxime Kohlhaas <maxime@atm-consulting.fr>
2014-10-24 15:45:27 +0200

    Fix : hook class name was not correctly replaced by script due to lowercase

--- class/actions_mymodule.class.php
+++ class/actions_mymodule.class.php
@@ -1,5 +1,5 @@
 <?php
-class ActionsMymodule
+class ActionsMyModule
 { 
      /** Overloading the doActions function : replacing the parent's function with the one below 
       *  @param      parameters  meta datas of the hook (context, etc...) 
