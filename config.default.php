335e8c0d6eddfb62b3a5375b692b58aee981278d
Alexis Algoud <alexis@atm-consulting.fr>
2015-02-12 11:24:52 +0100

    Correction lien

--- config.default.php
+++ config.default.php
@@ -23,7 +23,7 @@
 	}
 
 	if(!dol_include_once('/abricot/inc.core.php')) {
-		print $langs->trans('AbricotNotFound'). ' : <a href="http://wiki.atm-consulting.fr/index.php/Accueil#Module_Abricot" target="_blank">Abricot</a>';
+		print $langs->trans('AbricotNotFound'). ' : <a href="http://wiki.atm-consulting.fr/index.php/Accueil#Abricot" target="_blank">Abricot</a>';
 		exit;
 	}
 
