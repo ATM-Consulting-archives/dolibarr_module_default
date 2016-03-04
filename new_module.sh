b021cb923fac3cbf1f78569de437bb352aef5ffd
Alexis Algoud <alexis@atm-consulting.fr>
2016-03-04 10:15:37 +0100

    min

--- new_module.sh
+++ new_module.sh
@@ -1,4 +1,4 @@
-default_folder=$(pwd)							# On garde le chemin vers le répertoire contenant le module default
+*default_folder=$(pwd)							# On garde le chemin vers le répertoire contenant le module default
 cd ..											# On remonte d'un cran
 
 new_module=$1
@@ -27,5 +27,6 @@ do
 	mv $OLDNAME $NEWNAME
 done
 
+git init
 
 echo "Nouveau module $new_module préparé. Merci de supprimer le répertoire .git"
