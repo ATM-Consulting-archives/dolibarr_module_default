3e1a431a8df22a4b06f93f3084ead1d4590c8925
phf <phf@atm-consulting.fr>
2017-02-20 10:53:51 +0100

    Update add form pour ajax_constantonoff si pas de js

--- admin/mymodule_setup.php
+++ admin/mymodule_setup.php
@@ -124,7 +124,11 @@ print '<tr '.$bc[$var].'>';
 print '<td>'.$langs->trans("ParamLabel").'</td>';
 print '<td align="center" width="20">&nbsp;</td>';
 print '<td align="center" width="300">';
+print '<form method="POST" action="'.$_SERVER['PHP_SELF'].'">'; // Keep form because ajax_constantonoff return single link with <a> if the js is disabled
+print '<input type="hidden" name="token" value="'.$_SESSION['newtoken'].'">';
+print '<input type="hidden" name="action" value="set_CONSTNAME">';
 print ajax_constantonoff('CONSTNAME');
+print '</form>';
 print '</td></tr>';
 
 print '</table>';
