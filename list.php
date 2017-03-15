94963b28f185bd656f8e83d80d7758869572514f
phf <phf@atm-consulting.fr>
2017-03-15 14:25:26 +0100

    New list.php

---
+++ list.php
@@ -0,0 +1,115 @@
+<?php
+
+require 'config.php';
+dol_include_once('/mymodule/class/mymodule.class.php');
+
+if(empty($user->rights->mymodule->read)) accessforbidden();
+
+$langs->load('abricot@abricot');
+$langs->load('mymodule@mymodule');
+
+$PDOdb = new TPDOdb;
+$object = new TMyModule;
+
+$hookmanager->initHooks(array('mymodulelist'));
+
+/*
+ * Actions
+ */
+
+$parameters=array();
+$reshook=$hookmanager->executeHooks('doActions',$parameters,$object);    // Note that $action and $object may have been modified by some hooks
+if ($reshook < 0) setEventMessages($hookmanager->error, $hookmanager->errors, 'errors');
+
+if (empty($reshook))
+{
+	// do action from GETPOST ... 
+}
+
+
+/*
+ * View
+ */
+
+llxHeader('',$langs->trans('listMyModule'),'','');
+
+//$type = GETPOST('type');
+//if (empty($user->rights->mymodule->all->read)) $type = 'mine';
+
+// TODO ajouter les champs de son objet que l'on souhaite afficher
+$sql = 'SELECT t.rowid, t.date_cre, t.date_maj, \'\' AS action';
+
+$sql.= ' FROM '.MAIN_DB_PREFIX.'mymodule t ';
+
+$sql.= ' WHERE 1=1';
+//$sql.= ' AND t.entity IN ('.getEntity('MyModule', 1).')';
+//if ($type == 'mine') $sql.= ' AND t.fk_user = '.$user->id;
+
+
+$formcore = new TFormCore($_SERVER['PHP_SELF'], 'form_list_mymodule', 'GET');
+
+$nbLine = !empty($user->conf->MAIN_SIZE_LISTE_LIMIT) ? $user->conf->MAIN_SIZE_LISTE_LIMIT : $conf->global->MAIN_SIZE_LISTE_LIMIT;
+
+$r = new TListviewTBS($object);
+$r->render($PDOdb, $sql, array(
+	'view_type' => 'list' // default = [list], [raw], [chart]
+	,'limit'=>array(
+		'nbLine' => $nbLine
+	)
+	,'subQuery' => array()
+	,'link' => array()
+	,'type' => array(
+		'date_cre' => 'date' // [datetime], [hour], [money], [number], [integer]
+		,'date_maj' => 'date'
+	)
+	,'search' => array(
+		'date_cre' => array('recherche' => 'calendars', 'allow_is_null' => true)
+		,'date_maj' => array('recherche' => 'calendars', 'allow_is_null' => false)
+//		,'label' => array('recherche' => true, 'table' => array('t', 't'), 'field' => array('label', 'description')) // input text de recherche sur plusieurs champs
+//		,'status' => array('recherche' => array(0 => 'Draft', 1 => 'Validate', 2 => 'Closed'), 'to_translate' => true) // select html, la clé = le status de l'objet, 'to_translate' à true si nécessaire
+	)
+	,'translate' => array()
+	,'hide' => array(
+		'rowid'
+	)
+	,'liste' => array(
+		'titre' => $langs->trans('ListMyModule')
+		,'image' => img_picto('','title_generic.png', '', 0)
+		,'picto_precedent' => '<'
+		,'picto_suivant' => '>'
+		,'noheader' => 0
+		,'messageNothing' => $langs->trans('NoMyModule')
+		,'picto_search' => img_picto('','search.png', '', 0)
+	)
+	,'title'=>array(
+		'date_cre' => $langs->trans('DateCre')
+		,'date_maj' => $langs->trans('DateMaj')
+	)
+	,'eval'=>array(
+//		'fk_user' => '_getUserNomUrl(@val@)' // Si on a un fk_user dans notre requête
+	)
+));
+
+$parameters=array('sql'=>$sql);
+$reshook=$hookmanager->executeHooks('printFieldListFooter', $parameters, $object);    // Note that $action and $object may have been modified by hook
+print $hookmanager->resPrint;
+
+$formcore->end_form();
+
+llxFooter('');
+
+/**
+ * TODO remove if unused
+ */
+function _getUserNomUrl($fk_user)
+{
+	global $db;
+	
+	$u = new User($db);
+	if ($u->fetch($fk_user) > 0)
+	{
+		return $u->getNomUrl(1);
+	}
+	
+	return '';
+}
