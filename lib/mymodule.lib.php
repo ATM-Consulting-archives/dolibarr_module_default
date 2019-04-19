500f6b3e9b5f145b0f7aa97948d1a72bd20b8c89
John BOTELLA <john.botella@atm-consulting.fr>
2019-04-19 14:03:56 +0200

    Add extrafields

--- lib/mymodule.lib.php
+++ lib/mymodule.lib.php
@@ -40,6 +40,10 @@ function mymoduleAdminPrepareHead()
     $head[$h][1] = $langs->trans("About");
     $head[$h][2] = 'about';
     $h++;
+    $head[$h][0] = dol_buildpath("/mymodule/admin/mymodule_extrafields.php", 1);
+    $head[$h][1] = $langs->trans("ExtraFields");
+    $head[$h][2] = 'extrafields';
+    $h++;
 
     // Show more tabs from modules
     // Entries must be declared in modules descriptor with line
