9e1c3b47f9fcfbe740ffe0a4ba2864f37d5157d6
atm-ph <phf@atm-consulting.fr>
2019-06-19 17:23:53 +0200

    Fix missing right

--- core/modules/modMyModule.class.php
+++ core/modules/modMyModule.class.php
@@ -196,6 +196,13 @@ class modMyModule extends DolibarrModules
 		$this->rights[$r][4] = 'write';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
 		$this->rights[$r][5] = '';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
 		$r++;
+
+		$this->rights[$r][0] = $this->numero . $r;	// Permission id (must not be already used)
+		$this->rights[$r][1] = 'mymodule_delete';	// Permission label
+		$this->rights[$r][3] = 1; 					// Permission by default for new user (0/1)
+		$this->rights[$r][4] = 'delete';		    // In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
+		$this->rights[$r][5] = '';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
+		$r++;
 */
 
 		// Main menu entries
