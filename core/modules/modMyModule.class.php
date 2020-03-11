6eaa8849ba72a95de6c5d3dffe477fda87648a2d
Marc de Lima Lucio <marc.delimalucio@atm-consulting.fr>
2020-03-11 17:06:06 +0100

    FIX: module number with an extra zero

--- core/modules/modMyModule.class.php
+++ core/modules/modMyModule.class.php
@@ -47,7 +47,7 @@ class modMyModule extends DolibarrModules
 
 		// Id for module (must be unique).
 		// Use here a free id (See in Home -> System information -> Dolibarr for list of used modules id).
-		$this->numero = 1000000; // 104000 to 104999 for ATM CONSULTING
+		$this->numero = 100000; // 104000 to 104999 for ATM CONSULTING
 		// Key text used to identify module (for permissions, menus, etc...)
 		$this->rights_class = 'mymodule';
 
